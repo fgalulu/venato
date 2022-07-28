e85f425bace23847f67565362933f1dde3b93161
fgalulu <fionasonny132@gmail.com>
2022-07-28 22:45:24 +0300

    admin tests update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from flask import appcontext_popped
 import pytest
 from api import create_app, db, bcrypt
-from api.models import User, Role
+from api.models import User, Role, Project
 
 
 @pytest.fixture()
@@ -41,6 +41,25 @@ def new_admin(client):
 
 
 @pytest.fixture()
+def new_pm(client):
+    pwd = bcrypt.generate_password_hash('Pass1234!').decode('utf-8')
+    pm = User(first_name='project', last_name='manager', email='two@example.com', password=pwd,
+                 role=Role.PROJECT_MANAGER)
+    db.session.add(pm)
+    db.session.commit()
+    yield pm
+
+
+@pytest.fixture()
 def authorised_admin(new_admin):
     token = new_admin.get_token()
     yield token
+
+
+@pytest.fixture()
+def new_project(client, new_pm, new_admin):
+    project = Project(name='project 1', description='some description', supervisor=new_pm,
+                      project_author=new_admin)
+    db.session.add(project)
+    db.session.commit()
+    yield project
--- tests/functional_test/test_admin.py
+++ tests/functional_test/test_admin.py
@@ -111,7 +111,7 @@ def test_put_user_authorised(client, authorised_admin, new_user):
     assert response.status_code == 200
 
 
-def test_put_user_unauthorised(client, authorised_admin, new_user):
+def test_put_user_unauthorised(client, new_user):
     """
     Put an existing user with invalid token and valid data
     """
@@ -126,3 +126,163 @@ def test_put_user_unauthorised(client, authorised_admin, new_user):
     })
 
     assert response.status_code == 401
+
+
+
+def test_put_none_existing_user_authorised(client, authorised_admin, new_user):
+    """
+    Put an existing user with invalid token and valid data
+    """
+    payload = {
+        'email': 'test@email.com',
+        'first_name': 'test',
+        'last_name': 'one',
+        'password': ''
+    }
+    response = client.put(f'/admin/users/4', json=payload, content_type='application/json', headers={
+        'Authorization': f'Bearer {authorised_admin}'
+    })
+
+    assert response.status_code == 404
+
+
+def test_delete_authorised(client, authorised_admin, new_user):
+    """
+    Delete an existing user with valid token and valid user
+    """
+    response = client.delete(f'/admin/users/{new_user.id}', headers={
+        'Authorization': f'Bearer {authorised_admin}'
+    })
+
+    assert response.status_code == 200
+
+
+def test_delete_unauthorised(client, new_user):
+    """
+    Delete an existing user with valid token and valid user
+    """
+    response = client.delete(f'/admin/users/{new_user.id}', headers={
+        'Authorization': f'Bearer bejfnisomnxc625jsejf'
+    })
+
+    assert response.status_code == 401
+
+
+def test_delete_none_existing_user_authorised(client, authorised_admin, new_user):
+    """
+    Delete an existing user with valid token and valid user
+    """
+    response = client.delete(f'/admin/users/7', headers={
+        'Authorization': f'Bearer {authorised_admin}'
+    })
+
+    assert response.status_code == 404
+
+
+def test_get_single_project_authorised(client, authorised_admin, new_project):
+    """
+    Get single project with valid token
+    """
+    response = client.get(f'/admin/projects/{new_project.id}', headers={
+                'Authorization': f'Bearer {authorised_admin}'
+            })
+
+    assert response.status_code == 200
+
+
+def test_get_single_project_unauthorised(client, authorised_admin, new_project):
+    """
+    Get single project with valid token
+    """
+    response = client.get(f'/admin/projects/{new_project.id}', headers={
+                'Authorization': f'Bearer bejfnisomnxc625jsejf'
+            })
+
+    assert response.status_code == 401
+
+
+def test_get_none_existing_single_project_authorised(client, authorised_admin, new_project):
+    """
+    Get single project with valid token
+    """
+    response = client.get(f'/admin/projects/4', headers={
+                'Authorization': f'Bearer {authorised_admin}'
+            })
+
+    assert response.status_code == 404
+
+
+def test_get_all_projects_authorised(client, authorised_admin, new_project):
+    """
+    Get all projects with valid token
+    """
+    response = client.get(f'/admin/projects/', headers={
+                'Authorization': f'Bearer {authorised_admin}'
+            })
+
+    assert response.status_code == 200
+
+
+def test_get_all_projects_unauthorised(client):
+    """
+    Get all projects with valid token
+    """
+    response = client.get(f'/admin/projects/', headers={
+                'Authorization': f'Bearer bejfnisomnxc625jsejf'
+            })
+
+    assert response.status_code == 401
+
+
+def test_post_new_project_authorised(client, authorised_admin, new_pm):
+    """
+    Post new project with valid token
+    """
+    payload = {
+        'name': 'some project name',
+        'desc': 'some description for thw project',
+        'supervisor': f'{new_pm.id}'
+    }
+
+    response = client.post('/admin/projects/', json=payload, content_type='application/json', headers={
+        'Authorization': f'Bearer {authorised_admin}'
+    })
+
+    assert response.status_code == 200
+
+
+def test_post_new_project_unauthorised(client, new_pm):
+    """
+    Post new project with valid token
+    """
+    payload = {
+        'name': 'some project name',
+        'desc': 'some description for thw project',
+        'supervisor': f'{new_pm.id}'
+    }
+
+    response = client.post('/admin/projects/', json=payload, content_type='application/json', headers={
+        'Authorization': f'Bearer bejfnisomnxc625jsejf'
+    })
+
+    assert response.status_code == 401
+
+
+def test_post_new_project_with_none_pm_as_supervisor(client, authorised_admin, new_admin):
+    """
+    Post new project with valid token and assign supervisor a none pm user
+    """
+    payload = {
+        'name': 'some project name',
+        'desc': 'some description for thw project',
+        'supervisor': f'{new_admin.id}'
+    }
+
+    response = client.post('/admin/projects/', json=payload, content_type='application/json', headers={
+        'Authorization': f'Bearer {authorised_admin}'
+    })
+
+    assert response.status_code == 404
+
+
+# def test_post
